60f4110da11ec07da4c3a106edc26d0c6d36530c
ALabroo18 <102484252+ALabroo18@users.noreply.github.com>
2025-03-16 01:38:12 -0400

    Started Cleanup

--- TheLastVial_Project/Assets/Scripts/InteractObject.cs
+++ TheLastVial_Project/Assets/Scripts/InteractObject.cs
@@ -11,7 +11,7 @@ using Unity.VisualScripting;
 public class InteractObject : MonoBehaviour
 {
     public TextMeshProUGUI uiInteractionText;
-    public GameObject interactablePrefab;
+    private GameObject interactablePrefab;
 
     public CinemachineVirtualCamera myCamera;
 
@@ -42,9 +42,6 @@ public class InteractObject : MonoBehaviour
 
     public FirstPersonController fpc;
 
-    // public Cinemachine cineamachine
-
-
     // Bool to check if play is inside vent
 
     private bool isIn = false;
@@ -52,105 +49,43 @@ public class InteractObject : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        // Gets the first person player script to be used for playing sound
         fpc = this.GetComponent<FirstPersonController>();
-        // Ensure the vent sound does not play when the game starts
-        // if (ventSoundSource != null && ventSoundSource.isPlaying)
-        // {
-        //     ventSoundSource.Stop();
-        // }
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        if (hasCollided && Input.GetButtonDown("Fire1"))
+        if ((hasCollided && Input.GetButtonDown("Fire1")) && interactablePrefab != null)
         {
-            if (interactablePrefab != null)
-            {
-                if (isVentObject && !hasRotated)
-                {
-                    originalXRotation = interactablePrefab.transform.eulerAngles.x;
-                    newRotation = Quaternion.Euler(90, interactablePrefab.transform.eulerAngles.y, interactablePrefab.transform.eulerAngles.z);
-                    interactablePrefab.transform.rotation = newRotation;
-                    hasRotated = true;
-                    isVentObject = false;
-                    fpc.PlaySound(ventClip);  
-
-                    // Let it be open for a couple of seconds, then close
-                    StartCoroutine(CloseVent());
-                    
-
-                    fpc.PlaySound(ventClip);
-
-                    //Set has rotated to false
-                }
-                else
-                {
-                    if (!hasMoved)
-                    {
-                        // fpc.PlaySound(ventClip);
-                        Debug.Log("hasMoved is false");
-
-                        if(op != null)
-                            xMovementOffset = op.xvalue;
-                        interactablePrefab.transform.position = new Vector3(
-                            interactablePrefab.transform.position.x + xMovementOffset,
-                            interactablePrefab.transform.position.y,
-                            interactablePrefab.transform.position.z
-                        );
-                        hasMoved = true;
-                    }
-                    else if (hasMoved)
-                    {
-                        Debug.Log("hasMoved is true");
-                        interactablePrefab.transform.position = new Vector3(
-                            interactablePrefab.transform.position.x - xMovementOffset,
-                            interactablePrefab.transform.position.y,
-                            interactablePrefab.transform.position.z
-                        );
-                        hasMoved = false;
-                    }
-                }
-            }
+            ObjectState();
         }
-        // fpc.PlaySound(ventClip);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Whiteboard"))
-        {
-            Debug.Log("Whiteboard interacted");
-            op = collision.gameObject.GetComponent<PushValue>();
-            uiInteractionText.text = "Press the interact button (E) to Move Board";
-            interactablePrefab = collision.gameObject;
-
-            
-            hasCollided = true;
-        }
-        else if (collision.gameObject.CompareTag("Vent"))
-        {
-            uiInteractionText.text = "Press the interact Button (E) to Open Vent";
-            interactablePrefab = collision.gameObject;
-            hasCollided = true;
-            isVentObject = true;
-        }
+        CompareObject(collision);
     }
 
     private void OnCollisionExit(Collision collision)
     {
+
+        // Changes variables for when player leaves collision
         Debug.Log("Collision exited");
         uiInteractionText.text = "";
         hasCollided = false;
         if(collision.gameObject.CompareTag("Vent")) {
             isVentObject = false;
         }
-        // interactablePrefab = null;
     }
 
 
 
+
+
+// Changes the Cinemachine vertical Arm Length depending on condition
     private void ChangeCamera()
     {
         Cinemachine3rdPersonFollow followComponent = myCamera.GetCinemachineComponent<Cinemachine3rdPersonFollow>();
@@ -171,11 +106,83 @@ public class InteractObject : MonoBehaviour
 
         }
     }
+
+    private void ObjectState() {
+        if (isVentObject && !hasRotated)
+            {
+                originalXRotation = interactablePrefab.transform.eulerAngles.x;
+                newRotation = Quaternion.Euler(90, interactablePrefab.transform.eulerAngles.y, interactablePrefab.transform.eulerAngles.z);
+                interactablePrefab.transform.rotation = newRotation;
+                hasRotated = true;
+                isVentObject = false;
+                fpc.PlaySound(ventClip);  
+
+                // Let it be open for a couple of seconds, then close
+                StartCoroutine(CloseVent());
+                    
+
+                fpc.PlaySound(ventClip);
+
+                //Set has rotated to false
+            }
+
+            // If it is not a vent object and it hasnt rotated, then it will be a whiteboard
+            else
+            {
+                if (!hasMoved)
+                {
+                    Debug.Log("hasMoved is false");
+
+                    if(op != null)
+                        xMovementOffset = op.xvalue;
+                    interactablePrefab.transform.position = new Vector3(
+                    interactablePrefab.transform.position.x + xMovementOffset,
+                    interactablePrefab.transform.position.y,
+                    interactablePrefab.transform.position.z);
+                    hasMoved = true;
+                }
+                else if (hasMoved)
+                {
+                    Debug.Log("hasMoved is true");
+                    interactablePrefab.transform.position = new Vector3(
+                        interactablePrefab.transform.position.x - xMovementOffset,
+                        interactablePrefab.transform.position.y,
+                        interactablePrefab.transform.position.z);
+                    hasMoved = false;
+                }
+            }
+
+    }
+    
+    private void CompareObject(Collision collision) {
+        if (collision.gameObject.CompareTag("Whiteboard"))
+        {
+            Debug.Log("Whiteboard interacted");
+
+            // Gets the push value of the whiteboard (Each whiteboard has different push values)
+            op = collision.gameObject.GetComponent<PushValue>();
+            uiInteractionText.text = "Press the interact button (E) to Move Board";
+            interactablePrefab = collision.gameObject;
+
+          
+            hasCollided = true;
+        }
+        else if (collision.gameObject.CompareTag("Vent"))
+        {
+            uiInteractionText.text = "Press the interact Button (E) to Open Vent";
+            interactablePrefab = collision.gameObject;
+            hasCollided = true;
+
+            // To differentiate, there is another boolean that checks if the object is a vent
+            isVentObject = true;
+        }
+
+    }
     IEnumerator CloseVent()
     {
         ChangeCamera();
 
-        
+        // Waits for 3 seconds before closing vent
         yield return new WaitForSeconds(3);
 
         newRotation = Quaternion.Euler(originalXRotation, interactablePrefab.transform.eulerAngles.y, interactablePrefab.transform.eulerAngles.z);
